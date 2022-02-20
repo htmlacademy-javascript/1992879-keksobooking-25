26d6ceb8d6d3ea927f84cfbd6321f04a29924931
ivanou <anjinsan132131@gmail.com>
2022-02-20 19:28:39 +0300

    Исправляет функции

--- js/main.js
+++ js/main.js
@@ -1,5 +1,16 @@
-const getRandomNumber = (num1, num2) =>  Math.floor(num1 + Math.random() * (num2 + 1 - num1));
-const getRandomFractional = (num1, num2, fraction) => +Math.abs(Math.random() * (num1 - num2) + num2).toFixed(fraction);
+const getRandomInteger = (min, max) => {
+  const lowerValue = Math.min(min,max);
+  const upperValue = Math.max(min,max);
+  const result = Math.random() * (upperValue - lowerValue + 1) + lowerValue;
+  return Math.floor(result);
+};
 
-getRandomNumber();
-getRandomFractional();
+const getRandomFloatNumber = (min, max, fraction) => {
+  const lowerValue = Math.min(min,max);
+  const upperValue = Math.max(min,max);
+  const result = Math.random() * (upperValue - lowerValue + 1) + lowerValue;
+  return parseFloat(result.toFixed(fraction));
+};
+
+getRandomInteger(8, 66);
+getRandomFloatNumber(66, 9, 3);
