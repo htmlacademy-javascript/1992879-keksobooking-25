4d4d08ae10ca301d2a93825b0a5deaee38fedcee
ivanou <anjinsan132131@gmail.com>
2022-03-15 22:25:08 +0300

    возвращает импорт

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import { similarAnnouncement } from '../js/data.js';
 import { renderCard } from '../js/card.js';
+import { activeState } from '../js/form.js';
 
 const cardWrapper = document.querySelector('#map-canvas');
 const [ firstApartData ] = similarAnnouncement;
 
-// eslint-disable-next-line no-console
-console.log(similarAnnouncement);
-
 renderCard(firstApartData, cardWrapper);
+activeState();
