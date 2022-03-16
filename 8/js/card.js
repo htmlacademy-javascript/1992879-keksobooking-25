d77cb7fc0eed6850b61cac979f468d43618c3551
Keks <keks@htmlacademy.ru>
2022-03-16 22:28:26 +0300

    :heavy_check_mark: Сборка #8

---
+++ 8/js/card.js
@@ -0,0 +1,86 @@
+import {
+  PRICE_FORMAT,
+  HOUSE_TYPES,
+  ROOMS_TEXT,
+  GUEST_TEXT,
+  CHECKIN_TEXT,
+  CHECKOUT_TEXT,
+  FEATURE_CLASS_NAME,
+  PHOTO_ATTRIBUTE,
+  PHOTO_ATTRIBUTE_VALUE,
+  ADS_CARDS_SELECTORS
+} from '../js/constants.js';
+import { fillTemplate, getTemplateNode, getTemplateNodesByMap } from '../js/util.js';
+
+const renderFeatureList = (features, featureTemplateList) => {
+  const modifierFeatures = features.map((feature) => `${FEATURE_CLASS_NAME}${feature}`);
+
+  return featureTemplateList.forEach((element) => {
+    const [,modifierFeature] = element.classList;
+    if (!modifierFeatures.includes(modifierFeature)) {
+      element.remove();
+    }
+  });
+};
+
+const createFotoElement = (photoPath) => {
+  const photoElement = document.createElement('img');
+  photoElement.className = 'popup__photo';
+  photoElement.setAttribute(PHOTO_ATTRIBUTE.WIDTH, PHOTO_ATTRIBUTE_VALUE.WIDTH);
+  photoElement.setAttribute(PHOTO_ATTRIBUTE.HEIGHT, PHOTO_ATTRIBUTE_VALUE.HEIGHT);
+  photoElement.setAttribute(PHOTO_ATTRIBUTE.ALT, PHOTO_ATTRIBUTE_VALUE.ALT);
+  photoElement.src = photoPath;
+  return photoElement;
+};
+
+const renderCard = ({ author, offer }, renderBlock) => {
+  const { avatar } = author;
+  const { title, address, price, type, rooms, guests, checkin, checkout, features, description, photos } = offer;
+  const cardElement = getTemplateNode('#card', '.popup').cloneNode(true);
+
+  const templatesNode = getTemplateNodesByMap(cardElement, ADS_CARDS_SELECTORS);
+
+  fillTemplate(templatesNode.title, title);
+  fillTemplate(templatesNode.address, address);
+  fillTemplate(templatesNode.price, `${price} ${PRICE_FORMAT}`);
+  fillTemplate(templatesNode.type, HOUSE_TYPES[type]);
+  fillTemplate(templatesNode.description, description);
+
+  if (rooms && guests) {
+    fillTemplate(templatesNode.capacity, `${rooms} ${ROOMS_TEXT} ${guests} ${GUEST_TEXT}`);
+  } else {
+    templatesNode.capacity.remove();
+  }
+
+  if (checkin && checkout) {
+    fillTemplate(templatesNode.time,`${CHECKIN_TEXT} ${checkin} ${CHECKOUT_TEXT} ${checkout}`);
+  } else {
+    templatesNode.time.remove();
+  }
+
+  if (features) {
+    const featureTemplateList = templatesNode.features.querySelectorAll('.popup__feature');
+    renderFeatureList(features, featureTemplateList);
+  } else {
+    templatesNode.features.remove();
+  }
+
+  if (photos) {
+    templatesNode.photos.innerHTML = '';
+    photos.forEach((photoPath) => {
+      templatesNode.photos.appendChild(createFotoElement(photoPath));
+    });
+  } else {
+    templatesNode.photos.remove();
+  }
+
+  if (avatar) {
+    templatesNode.avatar.src = avatar;
+  } else {
+    templatesNode.avatar.remove();
+  }
+
+  renderBlock.appendChild(cardElement);
+};
+
+export { renderCard };
---
+++ 8/js/form/form.js
@@ -0,0 +1,60 @@
+import { HOUSE_MIN_PRICE_VALUE, HOUSE_MAX_PRICE_VALUE, RoomsCapacityMap, ERROR_VALIDATION_TEXT } from './form-constants.js';
+
+const announcementForm = document.querySelector('.ad-form');
+const titleField = announcementForm.querySelector('#title');
+const houseTypeField = announcementForm.querySelector('#type');
+const priceField = announcementForm.querySelector('#price');
+const roomNumberField = announcementForm.querySelector('#room_number');
+const capacityField = announcementForm.querySelector('#capacity');
+const timeIn = announcementForm.querySelector('#timein');
+const timeOut = announcementForm.querySelector('#timeout');
+
+const pristine = new Pristine(announcementForm, {
+  classTo: 'ad-form__element',
+  errorClass: 'ad-form__element-invalid',
+  successTextClass: 'ad-form__element-valid',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error'
+});
+
+const validateTitle = (value) => value.length >= 30 && value.length <= 100;
+const validateMinPrice = (value) => value >= HOUSE_MIN_PRICE_VALUE[houseTypeField.value];
+const validateMaxPrice = (value) => value <= HOUSE_MAX_PRICE_VALUE;
+const validateRoomNumber = (value) => RoomsCapacityMap[roomNumberField.value].guests.includes(value);
+
+const getMinPriceErrorMessage = () => `${ERROR_VALIDATION_TEXT.PRICE_MIN} ${HOUSE_MIN_PRICE_VALUE[houseTypeField.value]}`;
+const getMaxPriceErrorMessage = () => `${ERROR_VALIDATION_TEXT.PRICE_MAX} ${HOUSE_MAX_PRICE_VALUE}`;
+const getRoomsNumberErrorMessage = () => RoomsCapacityMap[roomNumberField.value].messageError;
+
+const onPriceChange = () => {
+  priceField.placeholder = HOUSE_MIN_PRICE_VALUE[houseTypeField.value];
+  pristine.validate(priceField);
+};
+
+const validateTimeOut = () => {
+  timeOut.value = timeIn.value;
+  pristine.validate(timeIn);
+};
+
+const validateTimeIn = () => {
+  timeIn.value = timeOut.value;
+  pristine.validate(timeOut);
+};
+
+pristine.addValidator(titleField, validateTitle, ERROR_VALIDATION_TEXT.TITLE);
+pristine.addValidator(priceField, validateMinPrice, getMinPriceErrorMessage);
+pristine.addValidator(priceField, validateMaxPrice, getMaxPriceErrorMessage);
+pristine.addValidator(capacityField, validateRoomNumber, getRoomsNumberErrorMessage);
+
+houseTypeField.addEventListener('change', onPriceChange);
+roomNumberField.addEventListener('change', () => {
+  pristine.validate(capacityField);
+});
+timeIn.addEventListener('change', validateTimeOut);
+timeOut.addEventListener('change', validateTimeIn);
+
+announcementForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  pristine.validate();
+});
