ff9886d42ad8fc6f5dca4825a246a5807cfe7ac4
ivanou <anjinsan132131@gmail.com>
2022-04-07 20:29:37 +0300

    вносит дополнительные переменные

--- js/slider.js
+++ js/slider.js
@@ -1,16 +1,24 @@
 import { HOUSE_MIN_PRICE_VALUE } from '../js/form/form-constants.js';
 import { houseTypeField, priceField, validatePrice } from '../js/form/form.js';
 
+const SLIDER_RANGE_VALUE = {
+  MIN: 0,
+  MAX: 100000
+};
+const SLIDER_START_VALUE = 0;
+const SLIDER_STEP = 1;
+const SLIDER_CONNECT_TYPE = 'lower';
+
 const sliderElement = document.querySelector('.ad-form__slider');
 
 noUiSlider.create(sliderElement, {
   range: {
     min: HOUSE_MIN_PRICE_VALUE[houseTypeField.value],
-    max: 100000,
+    max: SLIDER_RANGE_VALUE.MAX,
   },
-  start: 0,
-  step: 1,
-  connect: 'lower',
+  start: SLIDER_START_VALUE,
+  step: SLIDER_STEP,
+  connect: SLIDER_CONNECT_TYPE,
 });
 
 sliderElement.noUiSlider.on('update', (value) => {
@@ -21,8 +29,8 @@ sliderElement.noUiSlider.on('update', (value) => {
 houseTypeField.addEventListener('change', () => {
   sliderElement.noUiSlider.updateOptions({
     range: {
-      min: 0,
-      max: 100000,
+      min: SLIDER_RANGE_VALUE.MIN,
+      max: SLIDER_RANGE_VALUE.MAX,
     },
   });
 });
